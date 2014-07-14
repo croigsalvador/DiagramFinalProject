b7a71fffdf9eae5f57f0ee422ada970637dc3c34
roxeman3 <roxeman3@gmail.com>
2014-07-14 15:24:46 +0200

    Clean Code XD

--- DiagramFinalProject/CRNodeListViewController.m
+++ DiagramFinalProject/CRNodeListViewController.m
@@ -7,17 +7,111 @@
 //
 
 #import "CRNodeListViewController.h"
+#import "Node+Model.h"
 
-@interface CRNodeListViewController ()
+
+
+@interface CRNodeListViewController ()<NSFetchedResultsControllerDelegate>
+
+@property (strong,nonatomic) NSFetchedResultsController *fetchedResultsController;
 
 @end
 
 @implementation CRNodeListViewController
 
-- (void)viewDidLoad
-{
+#pragma mark - ViewController Lifecycle
+
+- (void)viewDidLoad {
     [super viewDidLoad];
-    
+    if (self.managedDocument.documentState == UIDocumentStateNormal) {
+        
+    }
 }
+
+#pragma mark - IBAction Methods
+
+- (IBAction)addNewNode:(UIBarButtonItem *)sender {
+    Node * node = [Node createNodeInManagedObjectContext:self.managedDocument.managedObjectContext];
+    node.title = @"Prueba";
+}
+
+#pragma mark - TableView Datasource Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchedResultsController sections][section];
+    return [sectionInfo numberOfObjects];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MapCell" forIndexPath:indexPath];
+    Node *node = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    cell.textLabel.text = node.title;
+    
+    return cell;
+}
+
+#pragma mark - NSFetchedResultsController 
+
+- (NSFetchedResultsController *)fetchedResultsController {
+    if (!_fetchedResultsController) {
+
+        _fetchedResultsController = [[NSFetchedResultsController  alloc] initWithFetchRequest:[Node fetchAllNodesByTitle] managedObjectContext:self.managedDocument.managedObjectContext sectionNameKeyPath:nil cacheName:nil];
+        _fetchedResultsController.delegate = self;
+        
+        if (![_fetchedResultsController performFetch:nil]){
+            // hanfle error
+        }
+    }
+    return _fetchedResultsController;
+}
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller
+{
+    [self.tableView beginUpdates];
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type
+{
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath
+{
+    UITableView *tableView = self.tableView;
+    
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeUpdate:
+            break;
+            
+        case NSFetchedResultsChangeMove:
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView endUpdates];
+}
+
 
 @end
