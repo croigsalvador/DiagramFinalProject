cb5b23becf53bdbf2c83b30719040f84cbb8e528
roxeman3 <roxeman3@gmail.com>
2014-07-14 11:45:03 +0200

    First Commit. Core Data node subclass

---
+++ DiagramFinalProject/Node.h
@@ -0,0 +1,30 @@
+//
+//  Node.h
+//  DiagramFinalProject
+//
+//  Created by Carlos Roig Salvador on 14/07/14.
+//  Copyright (c) 2014 CRoigSalvador. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Node;
+
+@interface Node : NSManagedObject
+
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * text;
+@property (nonatomic, retain) NSNumber * shapeType;
+@property (nonatomic, retain) NSSet *childs;
+@property (nonatomic, retain) Node *parent;
+@end
+
+@interface Node (CoreDataGeneratedAccessors)
+
+- (void)addChildsObject:(Node *)value;
+- (void)removeChildsObject:(Node *)value;
+- (void)addChilds:(NSSet *)values;
+- (void)removeChilds:(NSSet *)values;
+
+@end
---
+++ DiagramFinalProject/Node.m
@@ -0,0 +1,19 @@
+//
+//  Node.m
+//  DiagramFinalProject
+//
+//  Created by Carlos Roig Salvador on 14/07/14.
+//  Copyright (c) 2014 CRoigSalvador. All rights reserved.
+//
+
+#import "Node.h"
+
+@implementation Node
+
+@dynamic title;
+@dynamic text;
+@dynamic shapeType;
+@dynamic childs;
+@dynamic parent;
+
+@end
